c47945957156ab9cbb4d2669a9de411d62534732
Zhang yunfei <2536180464@qq.com>
2024-08-05 20:35:53 +0800

    2.0 后端代码最新版

--- bigdata_visualization_spring_boot/src/main/java/com/example/demo/vo/InterfaceVo.java
+++ bigdata_visualization_spring_boot/src/main/java/com/example/demo/vo/InterfaceVo.java
@@ -4,4 +4,9 @@ public class InterfaceVo {
     public String s;
     public Integer page;
     public Integer size;
+
+    public static void main(String[] args) {
+        String path=System.getProperty("java.home");
+        System.out.println(path);
+    }
 }
--- bigdata_visualization_spring_boot/src/test/java/com/example/demo/DemoApplicationTests.java
+++ bigdata_visualization_spring_boot/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,27 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.repository.JoinerRepository;
-import com.example.demo.repository.NoticeRepository;
-import com.example.demo.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-	@Autowired
 
-	private UserRepository userRepository;
 
-	@Autowired
-	NoticeRepository noticeRepository;
-
-	@Autowired
-	JoinerRepository joinerRepository;
-	@Test
-	void contextLoads() {
-		System.out.println(joinerRepository.findByActivity_id(1));
-		//System.out.println(joinerRepository.findByUser_id(1));
-	}
 
 }
